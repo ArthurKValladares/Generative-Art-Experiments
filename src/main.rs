87a7ed5d970e607da447856dceb1d2aa2e1e9cc7
ArthurValladares <arthurkaukal@gmail.com>
2022-04-12 23:40:06 -0700

    Basic winit event loop and window

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,36 @@
 use easy_ash::ApplicationInfo;
+use winit::{dpi::LogicalSize, event::Event, event_loop::EventLoop, window::WindowBuilder};
 
 fn main() {
-    let app_info = ApplicationInfo::default();
-    println!("App info: {:#?}", app_info);
+    let app_title = "Generative Art";
+    let window_width = 1200;
+    let window_height = 700;
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title(app_title)
+        .with_inner_size(LogicalSize::new(
+            f64::from(window_width),
+            f64::from(window_height),
+        ))
+        .build(&event_loop)
+        .unwrap();
+
+    let app_info = ApplicationInfo::default().with_application_name(app_title);
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = winit::event_loop::ControlFlow::Poll;
+        match event {
+            Event::WindowEvent {
+                event: window_event,
+                ..
+            } => match window_event {
+                winit::event::WindowEvent::CloseRequested => {
+                    *control_flow = winit::event_loop::ControlFlow::Exit
+                }
+                _ => {}
+            },
+            _ => {}
+        }
+    });
 }
